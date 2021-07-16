47b85c7575c1c6cc58019aae3b9ff95e210198c5
VladimirMoor <81152453+VladimirMoor@users.noreply.github.com>
2021-07-16 19:51:58 +0300

    Day 89 finished

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    
+    @State private var isActive = true
     
     var body: some View {
         
@@ -25,6 +25,16 @@ struct ContentView: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                    Capsule()
+                        .fill(Color.black)
+                        .opacity(0.75)
+                    )
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: self.cards[index]) {
@@ -34,6 +44,14 @@ struct ContentView: View {
                         }
                             .stacked(at: index, in: self.cards.count)
                     }
+                }
+                .allowsHitTesting(timeRemaining > 0)
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(Color.white)
+                        .foregroundColor(Color.black)
+                        .clipShape(Capsule())
                 }
             }
             
@@ -59,14 +77,33 @@ struct ContentView: View {
             }
         }
         .onReceive(timer) { time in
+            guard self.isActive else { return }
             if self.timeRemaining > 0 {
                 self.timeRemaining -= 1
             }
         }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            if self.cards.isEmpty == false {
+                self.isActive = true
+            }
+        }
+        
     }
     
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = [Card](repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
